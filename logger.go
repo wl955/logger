1422cf8c2d404a4d572d764d891466f48a1e10be
jq <jq.go@qq.com>
2022-09-09 13:58:46 +0800

    code format

--- logger.go
+++ logger.go
@@ -1,19 +1,19 @@
 package logger
 
-func Error(args ...interface{}) {
-	logger.Error(args...)
-}
-
-func Errorf(template string, args ...interface{}) {
-	logger.Errorf(template, args...)
-}
-
 func Info(args ...interface{}) {
 	logger.Info(args...)
 }
 
 func Infof(template string, args ...interface{}) {
 	logger.Infof(template, args...)
+}
+
+func Error(args ...interface{}) {
+	logger.Error(args...)
+}
+
+func Errorf(template string, args ...interface{}) {
+	logger.Errorf(template, args...)
 }
 
 func Fatal(args ...interface{}) {
