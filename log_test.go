a8847a1a73864792f5695adbb72d82227bc5d88b
jq <jq.go@qq.com>
2024-08-08 11:31:53 +0800

    debug

--- log_test.go
+++ log_test.go
@@ -3,6 +3,6 @@ package log
 import "testing"
 
 func TestError(t *testing.T) {
-	With("a", 1).With("b", 2).Warnf("%s=%d", "c", 3)
-	Warnf("%s=%d", "c", 3)
+	With("a", 1).With("s", 2).With("d", 3).With("f", 4).Warnf("%s=%d", "g", 5)
+	Warnf("%s=%d", "g", 5)
 }
